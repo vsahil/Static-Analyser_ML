af71d56d39bdb8dd7e88a754328e7c3929b0cf95
Sahil Verma <v.sahil1@gmail.com>
2019-08-03 20:51:38 +0200

    added gitignore and the script for rmate of important files

---
+++ open.sh
@@ -0,0 +1,8 @@
+rmate tensorflow/tensorflow/python/ops/nn_ops.py
+rmate tensorflow/tensorflow/python/ops/variables.py 
+rmate tensorflow/tensorflow/python/ops/random_ops.py
+rmate tensorflow/tensorflow/python/ops/array_ops.py
+rmate tensorflow/tensorflow/python/ops/math_ops.py
+rmate tensorflow/tensorflow/python/framework/tensor_shape.py
+rmate tensorflow/tensorflow/python/framework/ops.py
+rmate tensorflow/tensorflow/python/client/session.py
